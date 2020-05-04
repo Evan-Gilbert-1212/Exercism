e472d30e53cf681a157b36f6f483e33d39a47088
Evan L Gilbert <Evan.Gilbert1212@gmail.com>
2020-05-04 09:37:53 -0400

    Completed Resistor Colors Exercism

---
+++ csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class ResistorColor
+{
+  public static int ColorCode(string color)
+  {
+    var colorCodes = new Dictionary<string, int> {
+          {"black", 0},
+          {"brown", 1},
+          {"red", 2},
+          {"orange", 3},
+          {"yellow", 4},
+          {"green", 5},
+          {"blue", 6},
+          {"violet", 7},
+          {"grey", 8},
+          {"white", 9}
+        };
+
+    int colorValue;
+
+    colorCodes.TryGetValue(color, out colorValue);
+
+    return colorValue;
+  }
+
+  public static string[] Colors()
+  {
+    return new string[10] {
+      "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"
+    };
+  }
+}
